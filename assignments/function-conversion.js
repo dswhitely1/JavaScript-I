8bbd8e33a7afe48bbbb81fb901e178abf3c28a8f
Donald Whitely <dswhitely1@gmail.com>
2019-04-14 16:53:21 -0400

    fuction-conversions.js done

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,22 +4,36 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+const myFunction = () => {
+	console.log('Function was invoked!');
+};
 
+myFunction();
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+const anotherFunction = (param) => {
+	return param;
+};
 
+anotherFunction('Example');
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+const add = (param1, param2) => {
+	return param1 + param2;
+};
 
+add(1, 2);
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+const subtract = (param1, param2) => {
+	return param1 - param2;
+};
 
 // Stretch
 
@@ -28,3 +42,9 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map((num) => {
+	return num * 3;
+});
+
+console.log(triple);
